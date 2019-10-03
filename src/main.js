26c2db7d4dd77d57eac331155530bef7e4cb4ee0
marielysis <marielys11@yahoo.com>
2019-10-03 08:51:03 -0300

    Subir para recibir

--- src/main.js
+++ src/main.js
@@ -57,6 +57,7 @@
     let tqllr = document.getElementById('root');
 
     document.getElementById('searchData').addEventListener('click', function() {
+        tqllr.innerHTML = " ";
         const titleSearch = document.getElementById('searchBar').value;
         const request = new Request('http://www.omdbapi.com/?i=tt3896198&apikey=f37c3cde&s=' + titleSearch);
 
@@ -108,30 +109,32 @@
                 })
         })
         //------------------------   /SALA 1/  ------------------------------------------------------------
-        // Primera pelicula
+
+    // Primera pelicula
+    let contQA = document.getElementById('');
     document.getElementById("buttonRoom1").addEventListener("click", () => {
             fetch('http://www.omdbapi.com/?i=tt0120903&apikey=f37c3cde&s=')
                 .then(res => res.json())
                 .then(data => {
                     let contMovie1 = document.createElement("div");
                     contMovie1.className = "question";
-                    searchEl.appendChild(contMovie1);
+                    contQA.appendChild(contMovie1);
                     contMovie1.innerHTML += `<img src='https://m.media-amazon.com/images/M/MV5BZmIyMDk5NGYtYjQ5NS00ZWQxLTg2YzQtZDk1ZmM4ZDBlN2E3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg'>
                                         <h1>¿Quién es el director de la película X-Men?</h1>`
                     let imputQuestion1 = document.createElement("button");
                     imputQuestion1.className = "butQ";
                     imputQuestion1.textContent = "Opcion A: fkjhsdfgfkjdjsdvkdvkjnds";
-                    searchEl.appendChild(imputQuestion1);
+                    contQA.appendChild(imputQuestion1);
 
                     let imputQuestion2 = document.createElement("button");
                     imputQuestion2.className = "butQ";
                     imputQuestion2.textContent = "Opcion B: fkjhsdfgfkjdjsdvkdvkjnds";
-                    searchEl.appendChild(imputQuestion2);
+                    contQA.appendChild(imputQuestion2);
 
                     let imputQuestion3 = document.createElement("button");
                     imputQuestion3.className = "butQ";
                     imputQuestion3.textContent = "Opcion C: fkjhsdfgfkjdjsdvkdvkjnds";
-                    searchEl.appendChild(imputQuestion3);
+                    contQA.appendChild(imputQuestion3);
 
                     console.log(data);
                 }); //Cierre then data peli1 
