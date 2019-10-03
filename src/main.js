fa4613e0610b61b56a50744423a2ac1bdd7bae49
marielysis <marielys11@yahoo.com>
2019-10-02 22:22:02 -0300

    Varios cambios

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@ document.getElementById("buttonPlay").addEventListener("click", () => {
 
 
 
-
+//----SECCION DESCUBRIR-----------------------------------------------------------------------
 let searchEl = document.getElementById('searchResult');
 let contQA = document.getElementById('roomsContainer');
 
@@ -53,7 +53,7 @@ document.getElementById('searchData').addEventListener('click', function() {
         var len = data.Search.length;
         for (var i = 0; i < len; i++) {
             var movieContainer = document.createElement('div');
-            movieContainer.className = 'search-result--item';
+            movieContainer.className = 'results';
             var titleEl = document.createElement('div');
             titleEl.innerText = data.Search[i].Title;
             var yearEl = document.createElement('div');
@@ -70,6 +70,29 @@ document.getElementById('searchData').addEventListener('click', function() {
         }
     });
 });
+
+document.getElementById("buttonReviews").addEventListener("click", () => {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=71949b65aff64acdb6a0fce55fb7fb1d&sort_by=popularity.desc&page=1&primary_release_date.gte=2019-01-01')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            //let searchEl = document.getElementById("root");
+
+            const listmovie2 = data.results.map(element => {
+
+                return `<div class="results">
+                          <img src= "${'https://image.tmdb.org/t/p/w500'+element.poster_path}">  
+                          <h1> ${element.title}</h1>
+                          <h3>Year: ${element.release_date}</h3>
+                          <h4> Reseña: ${element.overview}</h4>
+                       </div>`
+            }).join('');
+
+            searchEl.innerHTML = listmovie2;
+        })
+})
+
+//------------------------SECCION TRIVIA-----------------------------------------------------------------------
 //------------------------   /SALA 1/  ------------------------------------------------------------
 // Primera pelicula
 document.getElementById("room1").addEventListener("click", () => {
@@ -97,7 +120,7 @@ document.getElementById("room1").addEventListener("click", () => {
                 imputQuestion3.textContent = "Opcion C: fkjhsdfgfkjdjsdvkdvkjnds";
                 contQA.appendChild(imputQuestion3);
 
-                //console.log(data);
+                console.log(data);
             }); //Cierre then data peli1 
     }) //Cierre sala 1
 
@@ -110,23 +133,23 @@ document.getElementById("room1").addEventListener("click", () => {
             .then(data => {
                 let contMovie = document.createElement("div");
                 contMovie.className = "question";
-                searchEl.appendChild(contMovie);
+                contQA.appendChild(contMovie);
                 contMovie.innerHTML += `<img src='https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg'>
                                 <h1>Quien es el director de la pelicula?</h1>`
                 let imputQuestion1 = document.createElement("button");
                 imputQuestion1.className = "butQ";
                 imputQuestion1.textContent = "Opcion A: fkjhsdfgfkjdjsdvkdvkjnds";
-                searchEl.appendChild(imputQuestion1);
+                contQA.appendChild(imputQuestion1);
 
                 let imputQuestion2 = document.createElement("button");
                 imputQuestion2.className = "butQ";
                 imputQuestion2.textContent = "Opcion B: fkjhsdfgfkjdjsdvkdvkjnds";
-                searchEl.appendChild(imputQuestion2);
+                contQA.appendChild(imputQuestion2);
 
                 let imputQuestion3 = document.createElement("button");
                 imputQuestion3.className = "butQ";
                 imputQuestion3.textContent = "Opcion C: fkjhsdfgfkjdjsdvkdvkjnds";
-                searchEl.appendChild(imputQuestion3);
+                contQA.appendChild(imputQuestion3);
 
                 console.log(data);
             }); //Cierre then data peli1 
